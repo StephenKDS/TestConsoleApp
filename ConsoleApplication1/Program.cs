a0b4cb3847db6a462ed3a3e9ddb83bb56a6fec36
Stephen Aitchison <stephen@kiwidevstudios.com>
2015-07-26 01:41:52 +1200

    First CheckIn for TestConsoleApp to GitHub

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World - Console application!");
+            Console.WriteLine("written to use as a test c# console application for BaaS!");
+            Console.WriteLine("-----");
+            Console.ReadLine();
+        }
+    }
+}
